5c24e40781de08d4b8ca91ae8a7573ba670a349d
TANJUMAJERIN <113289114+TANJUMAJERIN@users.noreply.github.com>
2023-05-24 20:23:22 -0700

    Add files via upload

--- SPL_codes/encode.h
+++ SPL_codes/encode.h
@@ -1,8 +1,69 @@
 #include<bits/stdc++.h>
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
+
+
 void encodeFile(const char* filePath) {
+    std::ifstream file(filePath, std::ios::binary);
+
+    if (!file.is_open()) {
+        std::cout << "Failed to open file!" << std::endl;
+        return;
+    }
+
+
+    std::vector<char> fileData((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    file.close();
+
+
+    for (char& byte : fileData) {
+        byte = byte << 1;
+    }
+
+    std::ofstream outFile(filePath, std::ios::binary);
+
+
+    outFile.write(fileData.data(), fileData.size());
+
+    outFile.close();
+    std::cout << "File encoding complete." << std::endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include<bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void decodeFile(const char* filePath) {
     std::fstream file(filePath, std::ios::in | std::ios::out | std::ios::binary);
 
     if (!file.is_open()) {
@@ -12,7 +73,7 @@ void encodeFile(const char* filePath) {
 
     char ch;
     while (file.read(&ch, sizeof(ch))) {
-        ch = ch >> 1; // Perform right shift by one bit
+        ch = ch << 1; // Perform left shift by one bit
 
         // Move the file pointer back by one byte to overwrite the original byte
         file.seekp(-sizeof(ch), std::ios::cur);
@@ -21,8 +82,6 @@ void encodeFile(const char* filePath) {
     }
 
     file.close();
-    cout<<"file quarentien complete"<<endl;
-}
-
-
+    std::cout << "file Decoding complete." << std::endl;
+}*/
 
